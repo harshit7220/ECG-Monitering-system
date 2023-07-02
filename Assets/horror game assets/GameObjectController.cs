3a89c16c594fa66a8e7f12ead19a514916228216
harshit7220 <82337178+harshit7220@users.noreply.github.com>
2023-07-02 22:19:42 +0530

    Assets

---
+++ Assets/horror game assets/GameObjectController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectController : MonoBehaviour
+{
+    [SerializeField] private GameObject audioAvatar;
+    [SerializeField] private bool doTurnoffAudioAvatar;
+    public List<GameObject> offGameObjects=new List<GameObject>();
+    public List<GameObject> onGameObjects = new List<GameObject>();
+  
+    private void Start()
+    {
+        Invoke(nameof(AnimDisable), audioAvatar.GetComponent<AudioSource>().clip.length);
+        Invoke(nameof(Enable), audioAvatar.GetComponent<AudioSource>().clip.length + 0.5f);
+        Invoke(nameof(Disable), audioAvatar.GetComponent<AudioSource>().clip.length + 0.5f);
+        Invoke(nameof(offAudio), audioAvatar.GetComponent<AudioSource>().clip.length + 0.5f);
+    }
+
+
+    private void Disable()
+    {
+        for (int i = 0; i < offGameObjects.Count; i++)
+        {
+            offGameObjects[i].SetActive(false);
+        }
+    }
+
+    private void Enable()
+    {
+        for (int i = 0; i < onGameObjects.Count; i++)
+        {
+            onGameObjects[i].SetActive(true);
+            Debug.Log(onGameObjects[i].name);
+        }
+    }
+
+    private void AnimDisable()
+    {
+        audioAvatar.GetComponent<Animator>().enabled = false;
+    }
+
+    private void offAudio()
+    {
+        if (doTurnoffAudioAvatar)
+        {
+            audioAvatar.SetActive(false);
+        }
+    }
+}
